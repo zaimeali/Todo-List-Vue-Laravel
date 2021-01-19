0daffb6b6029f1117736b7fabbafc5bc55e20765
zaimeali <zaimeali1997@gmail.com>
2021-01-19 17:24:41 +0500

    All Done

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <add-item-form 
+                v-on:reloadList="getList()"
+            />
+        </div>
+        <list-view 
+            :items="items" 
+            v-on:reloadList="getList()"
+        />
+    </div>
+</template>
+
+<script>
+    import AddItemForm from './addItemForm.vue'
+    import ListView from './listView.vue'
+    export default {
+        components: {
+            AddItemForm,
+            ListView,
+        },
+        data: () => {
+            return {
+                items: []
+            }
+        },
+        methods: {
+            getList() {
+                axios.get('api/items')
+                    .then(response => {
+                        this.items = response.data
+                    }).catch(error => console.log(error))
+            }
+        },
+        created() {
+            this.getList()
+        }
+    }
+</script>
+
+<style scoped>
+    .todoListContainer {
+        width: 350px;
+        margin: auto;
+    }
+
+    .heading {
+        background-color: #e6e6e6;
+        padding: 10px;
+    }
+
+    #title {
+        text-align: center;
+    }
+</style>
---
+++ resources/js/vue/listItem.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="item">
+        <input 
+            type="checkbox"
+            @change="updateCheck()"
+            v-model="item.completed"
+        />
+        <span :class="[ item.completed ? 'completed' : 'itemText' ]">{{ item.name }}</span>
+        <button @click="removeItem()" class="trashcan">
+            <font-awesome-icon 
+                icon="trash"
+            />
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['item'],
+        methods: {
+            updateCheck() {
+                axios.put('api/item/'+this.item.id, {
+                    item: this.item,
+                }).then(response => {
+                    if(response.status == 200) {
+                        this.$emit('itemChanged')
+                    }
+                }).catch(error => console.log(error))
+            },
+            removeItem() {
+                axios.delete('api/item/'+this.item.id)
+                    .then(response => {
+                        if(response.status == 200) {
+                            this.$emit('itemChanged')
+                        }
+                    }).catch(error => console.log(error))
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .item {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .completed {
+        text-decoration: line-through;
+        color: #999;
+    }
+    .itemText {
+        width: 100%;
+        margin-left: 20px;
+    }
+    .trashcan {
+        background: #e6e6e6;
+        border: none;
+        color: #FF0000;
+        outline: none;
+    }
+</style>
